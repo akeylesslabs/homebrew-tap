368fb4875c319e37b8de7f21edce10c7c691ab1f
OriMankali <53519283+OriMankali@users.noreply.github.com>
2022-04-30 20:25:51 +0300

    Update akeyless.rb

--- Formula/akeyless.rb
+++ Formula/akeyless.rb
@@ -4,18 +4,18 @@ class Akeyless < Formula
   version "1.47.0"
 
   if OS.mac? && Hardware::CPU.intel?
-    url "https://akeyless-cli.s3.us-east-2.amazonaws.com/cli/1.47.0/production/cli-darwin-amd64"
+    url "https://download.akeyless.io/Akeyless_Artifacts/MacOS/CLI/akeyless"
     sha256 "d3c1c9951283d23496d92cccf8eb0cdef6ff9fa4a1827b65b5d9a1d178a0a569"
     def install
-      bin.install "cli-darwin-amd64" => "akeyless"
+      bin.install "akeyless"
     end
   end
 
   if OS.mac? && Hardware::CPU.arm?
-    url "https://akeyless-cli.s3.us-east-2.amazonaws.com/cli/1.47.0/production/cli-darwin-arm64"
+    url "https://download.akeyless.io/Akeyless_Artifacts/MacOS/CLI/akeyless-arm"
     sha256 "172fe3c2f03f4c3808becc9f1c0587d8ce47e09a59d070021ca4ce3fb6a8fa06"
     def install
-      bin.install "cli-darwin-arm64" => "akeyless"
+      bin.install "akeyless-arm" => "akeyless"
     end
   end
 
