471bfa9b9733847619754055440a83658fc611fa
Ori Mankali <ori@akeyless.io>
2022-12-30 14:16:07 +0000

    brew tap update to version 1.61.0

--- Formula/akeyless.rb
+++ Formula/akeyless.rb
@@ -1,11 +1,11 @@
 class Akeyless < Formula
   desc "Akeyless CLI"
   homepage "https://www.akeyless.io"
-  version "1.60.0"
+  version "1.61.0"
 
   if OS.mac? && Hardware::CPU.intel?
     url "https://download.akeyless.io/Akeyless_Artifacts/MacOS/CLI/akeyless"
-    sha256 "6265a361e72f22830106da6d64ea9ab63ba4d7c47094add3491c8795110d50b7"
+    sha256 "66aadbda4465fed76ba5eb806148ef7c3a13ca9739075924c65f4b491200c9bc"
     def install
       bin.install "akeyless"
     end
@@ -13,7 +13,7 @@ class Akeyless < Formula
 
   if OS.mac? && Hardware::CPU.arm?
     url "https://download.akeyless.io/Akeyless_Artifacts/MacOS/CLI/akeyless-arm"
-    sha256 "d8ac7a5051f4ca471811f9d147a0d332516795e9e150ec46cd74932e5ab2fda2"
+    sha256 "e23b2d3091524b68e758ad922f6f978005c5df5e0a414bfefdab22d809e2e3e8"
     def install
       bin.install "akeyless-arm" => "akeyless"
     end
